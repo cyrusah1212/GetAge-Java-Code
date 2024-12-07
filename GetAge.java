a69b9893b8b4d2efc08df1cf3074327b1382d313
cyrusah1212 <148664282+cyrusah1212@users.noreply.github.com>
2024-12-07 20:07:13 +0800

    Add files via upload

---
+++ GetAge.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+
+public class GetAge
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int age;
+
+        System.out.println("What is your age?");
+        age = keyboard.nextInt();
+        System.out.println("Here is the age that you entered");
+        System.out.println(age);
+
+    }
+}
